bbf2ee253c97c9bfa761299e8634e36fa1cc9937
Olivier Goffart <ogoffart@woboq.com>
2018-03-23 17:22:11 +0100

    QVariantList

--- qmetaobject/src/qttypes.rs
+++ qmetaobject/src/qttypes.rs
@@ -2,6 +2,7 @@ extern crate std;
 use std::os::raw::c_char;
 use std::convert::From;
 use std::fmt::Display;
+use std::ops::{Index,IndexMut};
 
 cpp_class!(pub struct QByteArray, "QByteArray");
 impl<'a> From<&'a str> for QByteArray {
@@ -67,6 +68,11 @@ impl From<QByteArray> for QVariant {
         unsafe {cpp!([a as "QByteArray"] -> QVariant as "QVariant" { return QVariant(a); })}
     }
 }
+impl From<QVariantList> for QVariant {
+    fn from(a : QVariantList) -> QVariant {
+        unsafe {cpp!([a as "QVariantList"] -> QVariant as "QVariant" { return QVariant(a); })}
+    }
+}
 impl From<i32> for QVariant {
     fn from(a : i32) -> QVariant {
         unsafe {cpp!([a as "int"] -> QVariant as "QVariant" { return QVariant(a); })}
@@ -83,6 +89,97 @@ impl From<bool> for QVariant {
     }
 }
 
+cpp_class!(pub struct QVariantList, "QVariantList");
+impl QVariantList {
+    pub fn push(&mut self, value: QVariant) {
+        unsafe {cpp!([self as "QVariantList*", value as "QVariant"]
+            { self->append(value); }
+        )}
+    }
+    pub fn insert(&mut self, index: usize, element: QVariant) {
+        unsafe {cpp!([self as "QVariantList*", index as "size_t", element as "QVariant"]
+            { self->insert(index, element); }
+        )}
+    }
+    pub fn remove(&mut self, index: usize) -> QVariant {
+        unsafe {cpp!([self as "QVariantList*", index as "size_t"] -> QVariant as "QVariant"
+            { return self->takeAt(index); }
+        )}
+    }
+    pub fn len(&self) -> usize {
+        unsafe {cpp!([self as "QVariantList*"] -> usize as "size_t"
+            { return self->size(); }
+        )}
+    }
+}
+
+
+impl Index<usize> for QVariantList {
+    type Output = QVariant;
+    fn index(&self, index: usize) -> &QVariant {
+        assert!(index < self.len());
+        unsafe {cpp!([self as "QVariantList*", index as "size_t"] -> &QVariant as "const QVariant*"
+            { return &self->at(index); }
+        )}
+    }
+}
+impl IndexMut<usize> for QVariantList {
+    fn index_mut(&mut self, index: usize) -> &mut QVariant {
+        assert!(index < self.len());
+        unsafe {cpp!([self as "QVariantList*", index as "size_t"] -> &mut QVariant as "QVariant*"
+            { return &(*self)[index]; }
+        )}
+    }
+}
+
+pub struct QVariantListIterator<'a> {
+    list: &'a QVariantList,
+    index: usize,
+    size: usize
+}
+
+impl<'a> Iterator for QVariantListIterator<'a> {
+    type Item = &'a QVariant;
+    fn next(&mut self) -> Option<&'a QVariant> {
+        if self.index == self.size {
+            None
+        } else {
+            self.index+=1;
+            Some(&self.list[self.index-1])
+        }
+    }
+}
+
+impl<'a> IntoIterator for &'a QVariantList {
+    type Item = &'a QVariant;
+    type IntoIter = QVariantListIterator<'a>;
+
+    fn into_iter(self) -> QVariantListIterator<'a> {
+        QVariantListIterator::<'a> { list:self, index: 0, size: self.len() }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_qvariantlist() {
+        let mut q = QVariantList::default();
+        q.push(42.into());
+        q.push(QString::from("Hello").into());
+        q.push(QByteArray::from("Hello").into());
+        assert_eq!(q[0].to_qbytearray().to_string(), "42");
+        assert_eq!(q[1].to_qbytearray().to_string(), "Hello");
+        assert_eq!(q[2].to_qbytearray().to_string(), "Hello");
+        let x : Vec<QByteArray> = q.into_iter().map(|x| x.to_qbytearray()).collect();
+        assert_eq!(x[0].to_string(), "42");
+        assert_eq!(x[1].to_string(), "Hello");
+        assert_eq!(x[2].to_string(), "Hello");
+
+    }
+}
+
+
 cpp_class!(pub struct QModelIndex, "QModelIndex");
 impl QModelIndex {
     pub fn row(&self) -> i32 {
@@ -91,5 +188,6 @@ impl QModelIndex {
         }
     }
 }
+
 
 
