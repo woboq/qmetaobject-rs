09e75c0ba746873878790485db18fafdb0ef1c60
Olivier Goffart <ogoffart@woboq.com>
2018-04-20 09:45:14 +0200

    Move the metatype in its own module

--- qmetaobject/src/lib.rs
+++ qmetaobject/src/lib.rs
@@ -106,89 +106,6 @@ pub fn invoke_signal(object : *mut c_void, meta : *const QMetaObject, id : u32, 
     })}
 }
 
-pub fn register_metatype<T : 'static + Sized + Clone + Default>(name : &str) -> i32 {
-    let size = std::mem::size_of::<T>() as u32;
-
-    extern fn deleter_fn<T>(_v: Box<T>) { };
-    let deleter_fn : extern fn(_v: Box<T>) = deleter_fn;
-
-    extern fn creator_fn<T : Default + Clone>(c : *const T) -> Box<T> {
-        if c.is_null() { Box::new( Default::default() ) }
-        else { Box::new(unsafe { (*c).clone() }) }
-    };
-    let creator_fn : extern fn(c : *const T) -> Box<T> = creator_fn;
-
-    extern fn destructor_fn<T>(ptr : *mut T) { unsafe { std::ptr::read(ptr); } };
-    let destructor_fn : extern fn(ptr : *mut T) = destructor_fn;
-
-    extern fn constructor_fn<T : Default + Clone>(dst : *mut T, c : *const T) -> *mut T {
-        unsafe {
-            let n = if c.is_null() {  Default::default() }
-                    else { (*c).clone() };
-            std::ptr::write(dst, n);
-        }
-        dst
-    };
-    let constructor_fn : extern fn(ptr : *mut T, c : *const T) -> *mut T = constructor_fn;
-
-    let name = std::ffi::CString::new(name).unwrap();
-    let type_id = unsafe {
-        let name = name.as_ptr();
-        cpp!([name as "const char*", size as "int", deleter_fn as "QMetaType::Deleter",
-                   creator_fn as "QMetaType::Creator", destructor_fn as "QMetaType::Destructor",
-                   constructor_fn as "QMetaType::Constructor"] -> i32 as "int" {
-            return QMetaType::registerType(name, deleter_fn, creator_fn, destructor_fn,
-                constructor_fn, size,
-                QMetaType::NeedsConstruction | QMetaType::NeedsDestruction | QMetaType::MovableType,
-                nullptr);
-        })
-    };
-
-    use std::any::TypeId;
-    if TypeId::of::<String>() == TypeId::of::<T>() {
-        extern fn converter_fn1(_ : *const c_void, s: &String, ptr : *mut QByteArray) {
-            unsafe { std::ptr::write(ptr, QByteArray::from(&*s as &str)); }
-        };
-        let converter_fn1: extern fn(_ : *const c_void, s: &String, ptr : *mut QByteArray) = converter_fn1;
-        extern fn converter_fn2(_ : *const c_void, s: &QByteArray, ptr : *mut String) {
-            unsafe { std::ptr::write(ptr, s.to_string()); }
-        };
-        let converter_fn2: extern fn(_ : *const c_void, s: &QByteArray, ptr : *mut String) = converter_fn2;
-        extern fn converter_fn3(_ : *const c_void, s: &String, ptr : *mut QString) {
-            let s : &str = &*s;
-            unsafe { std::ptr::write(ptr, QString::from(&*s as &str)); }
-        };
-        let converter_fn3: extern fn(_ : *const c_void, s: &String, ptr : *mut QString) = converter_fn3;
-        extern fn converter_fn4(_ : *const c_void, s: &QString, ptr : *mut String) {
-            unsafe { std::ptr::write(ptr, s.to_string()); }
-        };
-        let converter_fn4: extern fn(_ : *const c_void, s: &QString, ptr : *mut String) = converter_fn4;
-
-
-        unsafe { cpp!([type_id as "int",
-                    converter_fn1 as "QtPrivate::AbstractConverterFunction::Converter",
-                    converter_fn2 as "QtPrivate::AbstractConverterFunction::Converter",
-                    converter_fn3 as "QtPrivate::AbstractConverterFunction::Converter",
-                    converter_fn4 as "QtPrivate::AbstractConverterFunction::Converter"] {
-            //FIXME, the ConverterFunctor are gonna be leaking
-            auto c = new QtPrivate::ConverterFunctor<TraitObject, TraitObject, TraitObject>(converter_fn1);
-            if (!c->registerConverter(type_id, QMetaType::QByteArray))
-                delete c;
-            c = new QtPrivate::ConverterFunctor<TraitObject, TraitObject, TraitObject>(converter_fn2);
-            if (!c->registerConverter(QMetaType::QByteArray, type_id))
-                delete c;
-            c = new QtPrivate::ConverterFunctor<TraitObject, TraitObject, TraitObject>(converter_fn3);
-            if (!c->registerConverter(type_id, QMetaType::QString))
-                delete c;
-            c = new QtPrivate::ConverterFunctor<TraitObject, TraitObject, TraitObject>(converter_fn4);
-            if (!c->registerConverter(QMetaType::QString, type_id))
-                delete c;
-        }) };
-    }
-
-    type_id
-}
-
 #[repr(C)]
 pub struct QMetaObject {
     pub superdata : *const QMetaObject,
@@ -229,4 +146,6 @@ pub mod listmodel;
 pub use listmodel::*;
 pub mod qtdeclarative;
 pub use qtdeclarative::*;
+pub mod qmetatype;
+pub use qmetatype::*;
 
