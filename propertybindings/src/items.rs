4f509f8ab7590bcd20cd48e3f4373ca11adb8ae1
Olivier Goffart <ogoffart@woboq.com>
2018-07-28 13:53:41 +0200

    Integration with the scene graph

--- propertybindings/src/items.rs
+++ propertybindings/src/items.rs
@@ -2,9 +2,12 @@ use super::*;
 use std::rc::{Rc};
 use std::cell::{RefCell};
 
+use qmetaobject::scenegraph::{SGNode,ContainerNode,RectangleNode};
+use qmetaobject::{QColor, QQuickItem, QRectF};
 
 pub trait GeometryItem<'a> {
     fn geometry(&self) -> &Geometry<'a>;
+    fn update_paint_node(&self, node : SGNode<ContainerNode>, _item: &QQuickItem) -> SGNode<ContainerNode> { node }
 }
 
 #[derive(Default)]
@@ -27,8 +30,20 @@ pub struct ColumnLayout<'a> {
     geometry : Geometry<'a>,
     children: RefCell<Vec<Rc<GeometryItem<'a>>>>
 }
-impl<'a> GeometryItem<'a> for ColumnLayout<'a> { fn geometry(&self) -> &Geometry<'a> { &self.geometry } }
+impl<'a> GeometryItem<'a> for ColumnLayout<'a> {
+    fn geometry(&self) -> &Geometry<'a> { &self.geometry }
+
+    fn update_paint_node(&self, mut node : SGNode<ContainerNode>, item: &QQuickItem) -> SGNode<ContainerNode>
+    {
+        node.update_dynamic(self.children.borrow().iter(),
+            |i, n| i.update_paint_node(n, item) );
+        node
+    }
+
+}
 impl<'a> ColumnLayout<'a> {
+    pub fn new() -> Rc<Self> { Default::default() }
+
     pub fn add_child(&self, child : Rc<GeometryItem<'a>>) {
         self.children.borrow_mut().push(child);
         self.relayout();
@@ -86,6 +101,34 @@ fn test_layout() {
 }
 
 
+#[derive(Default)]
+pub struct Rectangle<'a> {
+    geometry : Geometry<'a>,
+    color: Property<'a, QColor>
+}
+impl<'a> GeometryItem<'a> for Rectangle<'a> {
+    fn geometry(&self) -> &Geometry<'a> { &self.geometry }
+    fn update_paint_node(&self, mut node : SGNode<ContainerNode>, item: &QQuickItem) -> SGNode<ContainerNode>
+    {
+        node.update_static(
+        ((|mut n : SGNode<RectangleNode>| -> SGNode<RectangleNode> {
+            n.create(item);
+            n.set_color(self.color.get());
+            let g = self.geometry();
+            n.set_rect(QRectF { x: g.left(), y:g.top(), width: g.width(), height: g.height()  });
+            n
+        })
+
+        ));
+        node
+    }
+
+}
+impl<'a> Rectangle<'a> {
+    pub fn new() -> Rc<Self> { Default::default() }
+}
+
+
 
 /*
 
@@ -132,3 +175,91 @@ rsml! {
 */
 
 //} // mod items
+
+
+use qmetaobject::{QObject};
+
+
+cpp!{{
+#include <QtQuick/QQuickItem>
+}}
+
+#[derive(QObject, Default)]
+struct QuickItem<'a> {
+    base: qt_base_class!(trait QQuickItem),
+    node : Option<Rc<GeometryItem<'a> + 'a>>,
+    init: qt_property!(bool; WRITE set_init)
+}
+impl<'a> QuickItem<'a> {
+
+    pub fn set_init(&mut self, _: bool) {
+        let i : Rc<GeometryItem<'a>> = rsml!(
+            Rectangle    {
+                geometry.width : 110.,
+                geometry.height : 90.,
+                color: QColor::from_name("blue"),
+            }
+        );
+        self.set_node(i);
+    }
+
+    pub fn set_node(&mut self, node: Rc<GeometryItem<'a> + 'a>) {
+        self.node = Some(node);
+        let obj = self.get_cpp_object();
+        assert!(!obj.is_null());
+        cpp!(unsafe [obj as "QQuickItem*"] { obj->setFlag(QQuickItem::ItemHasContents); })
+    }
+}
+
+
+impl<'a> QQuickItem for QuickItem<'a>
+{
+    fn update_paint_node(&mut self, mut node : SGNode<ContainerNode> ) -> SGNode<ContainerNode> {
+        if let Some(ref i) = self.node {
+            node = i.update_paint_node(node, self);
+        }
+        node
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test() {
+        use qmetaobject::*;
+        use super::QuickItem;
+        qml_register_type::<QuickItem>(cstr!("MyItem"), 1, 0, cstr!("MyItem"));
+        let mut engine = QmlEngine::new();
+        engine.load_data(r#"
+import QtQuick 2.0
+import QtQuick.Window 2.0
+import MyItem 1.0
+
+Window {
+    width: 800
+    height: 400
+    visible: true
+
+    Rectangle {
+        anchors.fill: parent
+        anchors.margins: 100
+        color: "red"
+        border.color: "black"
+        border.width: 2
+    }
+
+    MyItem {
+        anchors.fill: parent
+        anchors.margins: 100
+        init: true
+    }
+
+}
+
+
+
+        "#.into());
+        engine.exec();
+    }
+
+}
--- propertybindings/src/lib.rs
+++ propertybindings/src/lib.rs
@@ -1,5 +1,16 @@
+#[cfg(test)]
+#[macro_use] extern crate cstr;
+
+
+#[macro_use]
+extern crate qmetaobject;
+
+#[macro_use] extern crate cpp;
+
 #[macro_use]
 pub mod properties;
 pub use properties::*;
 pub mod anchors;
 pub mod items;
+
+
