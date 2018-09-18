5c73eb999ab949525bf50951bc2b9785e1b4129c
Olivier Goffart <ogoffart@woboq.com>
2018-09-18 16:53:32 +0200

    Add missing files

---
+++ examples/graph/build.rs
@@ -0,0 +1,73 @@
+/* Copyright (C) 2018 Olivier Goffart <ogoffart@woboq.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+extern crate cpp_build;
+use std::process::Command;
+
+fn qmake_query(var: &str) -> String {
+    String::from_utf8(
+        Command::new("qmake")
+            .args(&["-query", var])
+            .output()
+            .expect("Failed to execute qmake. Make sure 'qmake' is in your path")
+            .stdout,
+    ).expect("UTF-8 conversion failed")
+}
+
+fn main() {
+    let qt_include_path = qmake_query("QT_INSTALL_HEADERS");
+    let qt_library_path = qmake_query("QT_INSTALL_LIBS");
+
+    cpp_build::Config::new()
+        .include(qt_include_path.trim())
+        .include(qt_include_path.trim().to_owned() + "/QtQuick")
+        .include(qt_include_path.trim().to_owned() + "/QtCore")
+        .build("src/main.rs");
+
+    let macos_lib_search = if cfg!(target_os = "macos") {
+        "=framework"
+    } else {
+        ""
+    };
+    let macos_lib_framework = if cfg!(target_os = "macos") { "" } else { "5" };
+
+    println!(
+        "cargo:rustc-link-search{}={}",
+        macos_lib_search,
+        qt_library_path.trim()
+    );
+    println!(
+        "cargo:rustc-link-lib{}=Qt{}Widgets",
+        macos_lib_search, macos_lib_framework
+    );
+    println!(
+        "cargo:rustc-link-lib{}=Qt{}Gui",
+        macos_lib_search, macos_lib_framework
+    );
+    println!(
+        "cargo:rustc-link-lib{}=Qt{}Core",
+        macos_lib_search, macos_lib_framework
+    );
+    println!(
+        "cargo:rustc-link-lib{}=Qt{}Quick",
+        macos_lib_search, macos_lib_framework
+    );
+    println!(
+        "cargo:rustc-link-lib{}=Qt{}Qml",
+        macos_lib_search, macos_lib_framework
+    );
+}
---
+++ examples/graph/src/nodes.rs
@@ -0,0 +1,72 @@
+use qmetaobject::scenegraph::SGNode;
+use qmetaobject::{QColor, QQuickItem, QRectF};
+
+qrc!{
+    init_ressource,
+    "scenegraph/graph" {
+//        "main.qml",
+        "shaders/noisy.vsh",
+        "shaders/noisy.fsh",
+        "shaders/line.vsh",
+        "shaders/line.fsh",
+    }
+}
+
+// Ideally, everything should be possible to do in plain rust.
+// However, there is quite some API to expose.
+
+cpp!{{
+#include "src/linenode.cpp"
+#include "src/noisynode.cpp"
+#include "src/gridnode.cpp"
+#include <QtQuick/QQuickItem>
+}}
+
+pub enum NoisyNode {}
+
+pub fn create_noisy_node(s: &mut SGNode<NoisyNode>, ctx: &QQuickItem) {
+    init_ressource();
+    let item_ptr = ctx.get_cpp_object();
+    cpp!(unsafe [s as "NoisyNode**", item_ptr as "QQuickItem*"] {
+        if (!*s && item_ptr) {
+            *s = new NoisyNode(item_ptr->window());
+        }
+    });
+}
+
+pub fn noisy_node_set_rect(s: &mut SGNode<NoisyNode>, rect: QRectF) {
+    cpp!(unsafe [s as "NoisyNode**", rect as "QRectF"] {
+        if (*s) {
+            (*s)->setRect(rect);
+        }
+    });
+}
+
+pub enum GridNode {}
+pub fn update_grid_node(s: &mut SGNode<GridNode>, rect: QRectF) {
+    cpp!(unsafe [s as "GridNode**", rect as "QRectF"] {
+        if (!*s) *s = new GridNode;
+        (*s)->setRect(rect);
+    });
+}
+
+pub enum LineNode {}
+pub fn create_line_node(s: &mut SGNode<LineNode>, size: f32, spread: f32, color: QColor) {
+    init_ressource();
+    cpp!(unsafe [s as "LineNode**", size as "float", spread as "float", color as "QColor"] {
+        if (!*s) *s = new LineNode(size, spread, color);
+    });
+}
+
+pub fn update_line_node(s: &mut SGNode<LineNode>, rect: QRectF, samples: &[f64]) {
+    let samples_ptr = samples.as_ptr();
+    let samples_len = samples.len();
+    cpp!(unsafe [s as "LineNode**", rect as "QRectF", samples_ptr as "double*",
+                 samples_len as "std::size_t"] {
+        if (!*s) return;
+        QList<qreal> samples;
+        samples.reserve(samples_len);
+        std::copy(samples_ptr, samples_ptr + samples_len, std::back_inserter(samples));
+        (*s)->updateGeometry(rect, samples);
+    });
+}
