19d673dac048f7c01e0c203c2dd3eb1e94ff7d0c
Tamás Szelei <szelei.t@gmail.com>
2021-08-09 18:02:09 +0200

    Add qenum example

---
+++ examples/qenum/src/main.rs
@@ -0,0 +1,32 @@
+
+use cstr::cstr;
+
+use qmetaobject::prelude::*;
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, QEnum)]
+#[repr(u32)]
+enum Options {
+    Foo = 1,
+    Bar = 2,
+    Quaz = 3,
+}
+
+fn main() {
+    qml_register_enum::<Options>(cstr!("RustCode"), 1, 0, cstr!("Options"));
+    let mut engine = QmlEngine::new();
+    engine.load_data(r#"
+        import QtQuick 2.6
+        import QtQuick.Window 2.0
+        // Import our Rust classes
+        import RustCode 1.0
+
+        Window {
+            visible: true
+            Text {
+                anchors.centerIn: parent
+                text: `Hello! Bar is ${Options.Bar}, Foo is ${Options.Foo}.`
+            }
+        }
+    "#.into());
+    engine.exec();
+}
