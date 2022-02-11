d9ba5c2721757a1948814b228ecb329f83c93d3d
Ayush Singh <ayushsingh1325@gmail.com>
2022-02-12 01:11:06 +0530

    Refactor QStringList to qtcore::qlist::qstringlist

--- qttypes/src/qtcore/mod.rs
+++ qttypes/src/qtcore/mod.rs
@@ -1,11 +1,13 @@
 mod primitives;
 mod qbytearray;
 mod qchar;
+mod qlist;
 mod qstring;
 mod qurl;
 
 pub use self::primitives::qreal;
 pub use self::qbytearray::QByteArray;
 pub use self::qchar::UnicodeVersion;
+pub use self::qlist::QStringList;
 pub use self::qstring::{NormalizationForm, QString};
 pub use self::qurl::QUrl;
