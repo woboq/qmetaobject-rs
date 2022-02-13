c0d5a818297d80b46f8d4d4678bb59d03047d21c
Ayush Singh <ayushsingh1325@gmail.com>
2022-02-13 20:11:42 +0530

    Refactor QStringList tests.
    Also exposed QListIterator in qttypes.

--- qttypes/src/qtcore/qlist/mod.rs
+++ qttypes/src/qtcore/qlist/mod.rs
@@ -2,5 +2,6 @@ mod common;
 mod qstringlist;
 mod qvariantlist;
 
+pub use self::common::QListIterator;
 pub use self::qstringlist::QStringList;
 pub use self::qvariantlist::QVariantList;
--- qttypes/src/qtcore/qlist/qstringlist.rs
+++ qttypes/src/qtcore/qlist/qstringlist.rs
@@ -150,29 +150,6 @@ mod tests {
         qstringlist.insert(0, "Three".into());
         assert_eq!(qstringlist[0], QString::from("Three"));
 
-        assert_eq!(qstringlist, QStringList::from(["Three", "Two"]));
-        assert_eq!(qstringlist, QStringList::from(["Three".to_string(), "Two".to_string()]));
-        assert_eq!(qstringlist, QStringList::from([QString::from("Three"), QString::from("Two")]));
-
-        assert_eq!(qstringlist, QStringList::from(vec!["Three", "Two"]));
-        assert_eq!(qstringlist, QStringList::from(vec!["Three".to_string(), "Two".to_string()]));
-        assert_eq!(
-            qstringlist,
-            QStringList::from(vec![QString::from("Three"), QString::from("Two")])
-        );
-
-        let t = ["Three", "Two"];
-        assert_eq!(qstringlist, QStringList::from(t));
-        let t = ["Three".to_string(), "Two".to_string()];
-        assert_eq!(qstringlist, QStringList::from(t));
-        let t = [QString::from("Three"), QString::from("Two")];
-        assert_eq!(qstringlist, QStringList::from(t));
-
-        let temp: Vec<String> = qstringlist.clone().into();
-        assert_eq!(temp, vec!["Three".to_string(), "Two".to_string()]);
-        let temp: Vec<QString> = qstringlist.clone().into();
-        assert_eq!(temp, vec![QString::from("Three"), QString::from("Two")]);
-
         qstringlist.clear();
         assert_eq!(qstringlist.len(), 0);
     }
@@ -183,5 +160,52 @@ mod tests {
         let qvl: QStringList = v.clone().into_iter().collect();
         assert_eq!(qvl.len(), 3);
         assert_eq!(qvl[1].to_string(), v[1].to_string());
+    }
+
+    #[test]
+    fn test_vec_from_qstringlist() {
+        let qstringlist = generate_qstring_list();
+        let temp: Vec<String> = qstringlist.clone().into();
+        assert_eq!(temp, vec!["Three".to_string(), "Two".to_string()]);
+        let temp: Vec<QString> = qstringlist.clone().into();
+        assert_eq!(temp, vec![QString::from("Three"), QString::from("Two")]);
+    }
+
+    #[test]
+    fn test_qstringlist_from_slice_ref() {
+        let qstringlist = generate_qstring_list();
+        let t = ["Three", "Two"];
+        assert_eq!(qstringlist, QStringList::from(t));
+        let t = ["Three".to_string(), "Two".to_string()];
+        assert_eq!(qstringlist, QStringList::from(t));
+        let t = [QString::from("Three"), QString::from("Two")];
+        assert_eq!(qstringlist, QStringList::from(t));
+    }
+
+    #[test]
+    fn test_qstringlist_from_slice() {
+        let qstringlist = generate_qstring_list();
+
+        assert_eq!(qstringlist, QStringList::from(["Three", "Two"]));
+        assert_eq!(qstringlist, QStringList::from(["Three".to_string(), "Two".to_string()]));
+        assert_eq!(qstringlist, QStringList::from([QString::from("Three"), QString::from("Two")]));
+    }
+
+    #[test]
+    fn test_qstringlist_from_vec() {
+        let qstringlist = generate_qstring_list();
+        assert_eq!(qstringlist, QStringList::from(vec!["Three", "Two"]));
+        assert_eq!(qstringlist, QStringList::from(vec!["Three".to_string(), "Two".to_string()]));
+        assert_eq!(
+            qstringlist,
+            QStringList::from(vec![QString::from("Three"), QString::from("Two")])
+        );
+    }
+
+    fn generate_qstring_list() -> QStringList {
+        let mut qstringlist = QStringList::new();
+        qstringlist.push("Three".into());
+        qstringlist.push("Two".into());
+        return qstringlist;
     }
 }
