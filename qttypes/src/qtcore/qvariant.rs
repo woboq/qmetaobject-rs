fd532b860f1ae69a9f8bd9479c8cf019b11514f7
Matti Viljanen <matti.viljanen@kapsi.fi>
2024-02-23 00:10:34 +0200

    Add QVariant::to_qstring wrapper

--- qttypes/src/qtcore/qvariant.rs
+++ qttypes/src/qtcore/qvariant.rs
@@ -66,6 +66,15 @@ impl QVariant {
         })
     }
 
+    /// Wrapper around [`toString()`][method] method.
+    ///
+    /// [method]: https://doc.qt.io/qt-5/qvariant.html#toString
+    pub fn to_qstring(&self) -> QString {
+        cpp!(unsafe [self as "const QVariant*"] -> QString as "QString" {
+            return self->toString();
+        })
+    }
+
     // FIXME: do more wrappers
 }
 
